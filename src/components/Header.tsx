b43127171130a9e0d01b21bb7a9860d1ad89cc96
momu1372001-byte <your_email@example.com>
2025-09-27 20:33:01 +0300

    final update before build apk

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ interface HeaderProps {
 export const Header: React.FC<HeaderProps> = ({ activeTab, setActiveTab, language }) => {
   const texts = {
     ar: {
-      appName: " منظمMMMMMMMMM المهام",
+      appName: "منظم المهام",
       dashboard: "لوحة التحكم",
       tasks: "المهام",
       calendar: "التقويم",
